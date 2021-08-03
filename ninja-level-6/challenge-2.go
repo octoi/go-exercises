479cb9f9ee3c3eb5bad5e17ff7ba0e59e87d3470
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-03 21:48:25 +0530

    challenge 2

---
+++ ninja-level-6/challenge-2.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	n := foo(ii...)
+	fmt.Println(n)
+
+	n2 := bar(ii)
+	fmt.Println(n2)
+}
+
+func foo(xi ...int) int {
+	total := 0
+
+	for _, v := range xi {
+		total += v
+	}
+
+	return total
+}
+
+func bar(xi []int) int {
+	total := 0
+
+	for _, v := range xi {
+		total += v
+	}
+
+	return total
+}
